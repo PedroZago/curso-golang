d4534f3429a9b6a2c7d6cabe19935eb9f290019b
PedroZago <pedro.camargozago@hotmail.com.br>
2021-10-05 17:11:10 -0300

    Initial Commit

---
+++ 18 - Concorrência/3 - Canais/canais.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+	go escrever("Olá Mundo!", canal)
+
+	fmt.Println("Depois da função escrever começar a ser executada.")
+
+	for menssagem := range canal {
+		fmt.Println(menssagem)
+	}
+
+	fmt.Println("Fim do programa!")
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+
+	close(canal)
+}
